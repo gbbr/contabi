321cb859bc292dd93155ce73b2ffc4b6635ed2cc
Gabriel Aszalos <gabriel@sourcegraph.com>
2016-01-22 13:19:48 +0530

    Switched from HandlerFuncs to Handlers.

--- app/app.go
+++ app/app.go
@@ -12,22 +12,23 @@ func homePage(w http.ResponseWriter, r *http.Request) error {
 	return tmpl.ExecuteTemplate(w, "tmpl/files/home.html", struct{ Name string }{"James"})
 }
 
-func handleWithError(fn func(http.ResponseWriter, *http.Request) error) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		err := fn(w, r)
-		if err != nil {
-			// Handle error page
-			w.WriteHeader(http.StatusInternalServerError)
-			fmt.Fprintf(w, "error: %v", err)
-			return
-		}
+type handlerWithError struct {
+	fn func(http.ResponseWriter, *http.Request) error
+}
+
+func (h handlerWithError) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := h.fn(w, r)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "error: %v", err)
+		return
 	}
 }
 
 // Serve starts the HTTP server and listens for incoming page requests.
 func Serve() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", handleWithError(homePage))
+	mux.Handle("/", handlerWithError{homePage})
 
 	mux.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("./assets/js"))))
 	mux.Handle("/img/", http.StripPrefix("/img/", http.FileServer(http.Dir("./assets/img"))))
