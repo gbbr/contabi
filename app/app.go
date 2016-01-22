707f3c7ee12b75927f46840c357da17f2c4ed8d9
Gabriel Aszalos <gabriel@sourcegraph.com>
2016-01-22 12:40:50 +0530

    Added an app package for managing handlers/routes

---
+++ app/app.go
@@ -0,0 +1,34 @@
+package app
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gbbr/contabi/tmpl"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) error {
+	return tmpl.ExecuteTemplate(w, "tmpl/files/home.html", struct{ Name string }{"James"})
+}
+
+func handleWithError(fn func(http.ResponseWriter, *http.Request) error) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		err := fn(w, r)
+		if err != nil {
+			// Handle error page
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, "error: %v", err)
+			return
+		}
+	}
+}
+
+func Serve() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handleWithError(homePage))
+
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatal(err)
+	}
+}
--- cmd/contabi/main.go
+++ cmd/contabi/main.go
@@ -1,34 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-
-	"github.com/gbbr/contabi/tmpl"
-)
-
-func homePage(w http.ResponseWriter, r *http.Request) error {
-	return tmpl.ExecuteTemplate(w, "tmpl/files/home.html", struct{ Name string }{"James"})
-}
-
-func handleWithError(fn func(http.ResponseWriter, *http.Request) error) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		err := fn(w, r)
-		if err != nil {
-			// Handle error page
-			w.WriteHeader(http.StatusInternalServerError)
-			fmt.Fprintf(w, "error: %v", err)
-			return
-		}
-	}
-}
+import "github.com/gbbr/contabi/app"
 
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", handleWithError(homePage))
-
-	if err := http.ListenAndServe(":8080", mux); err != nil {
-		log.Fatal(err)
-	}
+	app.Serve()
 }
